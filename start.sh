f3a83dd482cf50221a610a85c33d1e5313cba544
Lionel Palacin <lionel.palacin@clickhouse.com>
2024-11-25 23:16:14 +0000

    update start script:

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Start Python API in the background
-cd py-embed && uvicorn app:app --port 8000 &
+cd py-embed && source venv/bin/activate && uvicorn app:app --port 8000 &
 
 # Start Next.js application
 cd /app && npm start -- -p 8080
